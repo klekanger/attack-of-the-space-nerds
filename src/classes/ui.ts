6450c2209d24ea017ad1591579b5718efae0b5e1
Kurt Lekanger <kurt@lekanger.no>
2022-11-21 21:37:52 +0100

    chore: add lives counter

--- src/classes/ui.ts
+++ src/classes/ui.ts
@@ -5,23 +5,23 @@ export class UI {
   fontSize: number;
   fontFamily: string;
   textColor: string;
-  ammoLevelColor: string;
 
   constructor(game: Game) {
     this.game = game;
     this.fontSize = 25;
     this.fontFamily = 'Press Start 2P';
     this.textColor = 'rgba(215 225 230 / 1)';
-    this.ammoLevelColor = 'rgba(222 189 65 / 0.5';
   }
 
   draw(context: CanvasRenderingContext2D) {
     context.save();
+
     context.fillStyle = this.textColor;
-    // context.font = this.fontSize + 'px ' + this.fontFamily;
     context.font = `${this.fontSize}px '${this.fontFamily}'`;
     context.textAlign = 'left';
+
     context.fillText(`Score: ${this.game.score}`, 20, 40);
+    context.fillText(`Lives: ${this.game.lives}`, this.game.width - 220, 40);
 
     context.restore();
   }
