adc8f86fab255d95e91c2e3c421fbebe1366ae81
Kurt Lekanger <kurt@lekanger.no>
2023-02-18 15:34:44 +0100

    refactor: use let, not var

--- src/lib/easing.ts
+++ src/lib/easing.ts
@@ -17,16 +17,16 @@ export function easeOutSine(t: number, b: number, c: number, d: number) {
 }
 
 export function easeInOutElastic(t: number, b: number, c: number, d: number) {
-  var s = 1.70158;
-  var p = 0;
-  var a = c;
+  let s = 1.70158;
+  let p = 0;
+  let a = c;
   if (t == 0) return b;
   if ((t /= d / 2) == 2) return b + c;
   if (!p) p = d * (0.3 * 1.5);
   if (a < Math.abs(c)) {
     a = c;
-    var s = p / 4;
-  } else var s = (p / (2 * Math.PI)) * Math.asin(c / a);
+    s = p / 4;
+  } else s = (p / (2 * Math.PI)) * Math.asin(c / a);
   if (t < 1)
     return (
       -0.5 *
