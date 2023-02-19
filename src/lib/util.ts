871082a945606ed828d8e867f6a6f70afaeb5865
Kurt Lekanger <kurt@lekanger.no>
2023-02-19 15:09:52 +0100

    feat: audio toggle

--- src/lib/util.ts
+++ src/lib/util.ts
@@ -7,7 +7,6 @@
  * @returns {number} return value
  *
  */
-
 export function randomBetween(min: number, max: number): number {
   return Math.random() * (max - min) + min;
 }
@@ -18,7 +17,6 @@ export function randomBetween(min: number, max: number): number {
  * @param {number} num Number to make positive or negative
  * @returns {number} A negative or positive version of `num`
  */
-
 export function makeRandomPositiveOrNegative(num: number): number {
   return num * (Math.round(Math.random()) * 2 - 1);
 }
@@ -29,7 +27,6 @@ export function makeRandomPositiveOrNegative(num: number): number {
  * @returns {boolean} true if user is on a mobile device
  *
  */
-
 export function isMobile(): boolean {
   return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
     navigator.userAgent
@@ -41,10 +38,9 @@ export function isMobile(): boolean {
  *
  * @param element A NodeList of HTMLElements
  */
-
 export function hideAllElements(element: NodeListOf<HTMLElement>) {
   element.forEach((e) => {
-    e.style.display = 'none';
+    e.style.display = "none";
   });
 }
 
@@ -53,9 +49,18 @@ export function hideAllElements(element: NodeListOf<HTMLElement>) {
  *
  * @param element A NodeList of HTMLElements
  */
-
 export function showAllElements(element: NodeListOf<HTMLElement>) {
   element.forEach((e) => {
-    e.style.display = 'block';
+    e.style.display = "block";
   });
+}
+
+/**
+ * Checks in localStorage if audio is on or off
+ *
+ * @returns {boolean} true if audio is on
+ */
+export function isAudioEnabled(): boolean {
+  const audio = localStorage.getItem("space_nerds_audio");
+  return audio === "on" ? true : false;
 }
--- src/main.ts
+++ src/main.ts
@@ -1,34 +1,68 @@
-import startScreenMusic from './audio/Raining Bits.ogg';
-import { Game } from './classes/game';
-import { hideAllElements, isMobile, showAllElements } from './lib/util';
-import { GameMode } from './types';
+import "@fortawesome/fontawesome-free/css/all.css";
+import { Game } from "./classes/game";
+import {
+  hideAllElements,
+  isAudioEnabled,
+  isMobile,
+  showAllElements,
+} from "./lib/util";
+import { GameMode } from "./types";
+import startScreenMusic from "/audio/Raining Bits.ogg";
 
-window.addEventListener('load', function () {
+const FA_AUDIO_OFF = `<i class="fa-solid fa-volume-xmark"></i>`;
+const FA_AUDIO_ON = `<i class="fa-solid fa-volume-high"></i>`;
+
+window.addEventListener("load", function () {
   // If mobile device, replace button text with "Tap to Play"
-  const playButton = document.getElementById('btn-play') as HTMLButtonElement;
+  const playButton = document.getElementById("btn-play") as HTMLButtonElement;
   isMobile()
-    ? (playButton.innerText = 'Tap to Play')
-    : (playButton.innerText = 'Space to Play');
+    ? (playButton.innerText = "Tap to Play")
+    : (playButton.innerText = "Space to Play");
 
   // Select elements to hide during game play
   const htmlToHideDuringPlay: NodeListOf<HTMLElement> =
-    document.querySelectorAll('.hide-during-play');
+    document.querySelectorAll(".hide-during-play");
 
+  // Set up audio and get audio toggle button element from DOM
   const introMusic = new Audio();
   introMusic.src = startScreenMusic;
   introMusic.loop = true;
-  introMusic.play();
+
+  const audioToggleButton = document.getElementById(
+    "speaker-symbol"
+  ) as HTMLElement;
+  audioToggleButton.innerHTML = FA_AUDIO_OFF;
+
+  // Check if audio is enabled in localStorage
+  if (isAudioEnabled()) {
+    introMusic.play();
+    audioToggleButton.innerHTML = FA_AUDIO_ON;
+  }
+
+  // Add event listener to audio toggle button
+  audioToggleButton.addEventListener("click", () => {
+    if (audioToggleButton.innerHTML === FA_AUDIO_OFF) {
+      audioToggleButton.innerHTML = FA_AUDIO_ON;
+      localStorage.setItem("space_nerds_audio", "on");
+      introMusic.play();
+    } else {
+      audioToggleButton.innerHTML = FA_AUDIO_OFF;
+      localStorage.setItem("space_nerds_audio", "off");
+      introMusic.pause();
+    }
+  });
 
   // Set up main game canvas
-  const canvas = document.getElementById('canvas1') as HTMLCanvasElement;
-  const context = canvas.getContext('2d');
-
+  const canvas = document.getElementById("canvas1") as HTMLCanvasElement;
+  const context = canvas.getContext("2d");
   canvas.width = 800;
   canvas.height = 900;
 
+  // Create game instance
+  // Almost all game logic is contained in the Game class
   const game = new Game(canvas, context);
 
-  playButton.addEventListener('click', () => {
+  playButton.addEventListener("click", () => {
     game.setGameMode(GameMode.PLAYING);
   });
 
@@ -51,11 +85,11 @@ window.addEventListener('load', function () {
 
     const gameMode = game.getGameMode();
 
-    if (gameMode === 'IDLE' && context) {
+    if (gameMode === "IDLE" && context) {
       game.splashScreen.update(delta);
       game.splashScreen.draw(context);
 
-      if (introMusic.paused) {
+      if (introMusic.paused && isAudioEnabled()) {
         introMusic.play();
       }
 
@@ -65,23 +99,26 @@ window.addEventListener('load', function () {
       }
     }
 
-    if (gameMode === 'DIETRANSITION' || gameMode === 'GAMEOVER') {
+    if (gameMode === "DIETRANSITION" || gameMode === "GAMEOVER") {
       // Clears all enemies and particles
       game.enemyWave.length !== 0 ? game.explodeAllEnemies() : null;
     }
 
     if (
-      gameMode === 'PLAYING' ||
-      gameMode === 'DIETRANSITION' ||
-      gameMode === 'GAMEOVER'
+      gameMode === "PLAYING" ||
+      gameMode === "DIETRANSITION" ||
+      gameMode === "GAMEOVER"
     ) {
       if (!introMusic.paused) {
         introMusic.pause();
       }
 
+      // Always run game.update and game.draw methods,
+      // except when IDLE (gameMode === 'IDLE')
+      // Then we should run the splash screen update and draw methods instead
       game.update(delta);
-
       if (context) game.draw(context);
+
       if (isStartTextVisible) {
         hideAllElements(htmlToHideDuringPlay);
         isStartTextVisible = false;
