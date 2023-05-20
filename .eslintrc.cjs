be0f743cac42322545fe6f9d2e254e575cdc03c5
Kurt Lekanger <kurt@lekanger.no>
2023-05-20 10:36:54 +0200

    build: not using React, remove from eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -11,10 +11,6 @@ module.exports = {
     "eslint-config-prettier",
   ],
   settings: {
-    react: {
-      // Tells eslint-plugin-react to automatically detect the version of React to use.
-      version: "detect",
-    },
     // Tells eslint how to resolve imports
     "import/resolver": {
       node: {
