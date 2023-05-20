8d43b3036d89a05193ecd6e71313192f0924220f
Kurt Lekanger <kurt@lekanger.no>
2023-05-20 10:39:06 +0200

    build: remove eslint-plugin-react

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,7 +2,6 @@ module.exports = {
   extends: [
     // By extending from a plugin config, we can get recommended rules without having to add them manually.
     "eslint:recommended",
-    "plugin:react/recommended",
     "plugin:import/recommended",
     "plugin:jsx-a11y/recommended",
     "plugin:@typescript-eslint/recommended",
